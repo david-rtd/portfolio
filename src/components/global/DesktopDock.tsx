f2a4a18910f9da65b5c58c5cb8a2e8b26d9287a5
David <ortegadorado1@gmail.com>
2026-06-11 00:31:39 +0200

    Cambio - David OrtegaDorado

--- src/components/global/DesktopDock.tsx
+++ src/components/global/DesktopDock.tsx
@@ -425,7 +425,7 @@ export default function DesktopDock() {
                       <div><span className='text-purple-400'>export const</span> <span className='text-blue-300'>developer</span> = {'{'}</div>
                       <div>  name: <span className='text-orange-300'>'David'</span>,</div>
                       <div>  role: <span className='text-orange-300'>'Cybersecurity & Full Stack'</span>,</div>
-                      <div>  tools: [<span className='text-orange-300'>'Astro'</span>, <span className='text-orange-300'>'React'</span>, <span className='text-orange-300'>'Node'</span>],</div>
+                      <div>  tools: [<span className='text-orange-300'>'Astro'</span>, <span className='text-orange-300'>'React'</span>, <span className='text-orange-300'>'Node'</span>, <span className='text-orange-300'>'python'</span>],</div>
                       <div>  security: {'{'}</div>
                       <div>    testing: [<span className='text-orange-300'>'Pentesting'</span>, <span className='text-orange-300'>'OWASP'</span>],</div>
                       <div>    scanners: [<span className='text-orange-300'>'Nmap'</span>, <span className='text-orange-300'>'Burp Suite'</span>]</div>
